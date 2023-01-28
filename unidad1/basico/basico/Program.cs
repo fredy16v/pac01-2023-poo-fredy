0de08386c2ed98fea4cee25bbb8d62557c0572c9
fredy16v <fredy16v@gmail.com>
2023-01-27 19:00:10 -0600

    Primer proyecto con tipos de datos.

---
+++ unidad1/basico/basico/Program.cs
@@ -0,0 +1,17 @@
+﻿using basico.tipo_datos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basico
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Tipos01 t01 = new Tipos01();
+        }
+    }
+}
